3fc91fec20c6e8fb2cc97b1b5a5f7b5f5c8c4e15
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2021-12-12 22:42:54 -0500

    added input and updated add task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 //createTaskHandler() function will dynamically create the task item
-var createTaskHandler = function () {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-}
+// var createTaskHandler = function () {
+//     var listItemEl = document.createElement("li");
+//     listItemEl.className = "task-item";
+//     listItemEl.textContent = "This is a new task.";
+//     tasksToDoEl.appendChild(listItemEl);
+// }
 //The following button will use createTaskHandler function as the callback function.
 buttonEl.addEventListener("click", createTaskHandler);
