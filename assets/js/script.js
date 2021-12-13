2552953abf28a3d7ae30fd104b8e24138e2b01b7
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2021-12-12 22:15:06 -0500

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//When button is clicked a new task is added
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+//createTaskHandler() function will dynamically create the task item
+var createTaskHandler = function () {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+//The following button will use createTaskHandler function as the callback function.
+buttonEl.addEventListener("click", createTaskHandler);
